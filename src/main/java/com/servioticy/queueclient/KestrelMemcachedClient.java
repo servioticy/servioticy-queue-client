bcdd611a8acbbae60fa3c3a0fc09d8d62bfa949c
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2014-02-14 18:18:18 +0100

    First content commit

---
+++ src/main/java/com/servioticy/queueclient/KestrelMemcachedClient.java
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright 2014 Barcelona Supercomputing Center (BSC)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/ 
+package com.servioticy.queueclient;
+
+import java.io.IOException;
+
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.ConnectionFactory;
+import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.FailureMode;
+import net.spy.memcached.MemcachedClient;
+
+import org.apache.commons.configuration.HierarchicalConfiguration;
+
+/**
+ * @author Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
+ * 
+ */
+public class KestrelMemcachedClient extends QueueClient {
+
+	private int expire = 0;
+
+	MemcachedClient kestrelClient;
+	
+	protected KestrelMemcachedClient() {
+	}
+
+	@Override
+	protected boolean putWrapper(Object item){
+		try{
+			// This is blocking by now
+			return kestrelClient.set(this.getRelativeAddress(), expire, item).get();
+		} catch (Exception e) {
+			logger.error("KestrelMemcached put failed ({})", e.getMessage());
+			return false;
+		} 
+		
+	}
+
+	@Override
+	protected void connectWrapper() throws QueueClientException {
+		if(kestrelClient != null){
+			return;
+		}
+		try {
+			ConnectionFactoryBuilder builder = new ConnectionFactoryBuilder();
+			// VERY IMPORTANT! Otherwise, spymemcached optimizes away concurrent gets
+	        builder.setShouldOptimize(false);
+	        // Retry upon failure
+	        builder.setFailureMode(FailureMode.Retry);
+	        ConnectionFactory memcachedConnectionFactory = builder.build();
+			kestrelClient = new MemcachedClient(
+					memcachedConnectionFactory,
+					AddrUtil.getAddresses(this.getBaseAddress()));
+		} catch (IOException e) {
+			kestrelClient = null;
+			String errMsg = "Unable to connect to the queue (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		}
+	}
+
+	@Override
+	protected void disconnectWrapper(){
+		if(kestrelClient == null){
+			return;
+		}
+
+		kestrelClient.shutdown();
+
+		kestrelClient = null;
+	}
+
+	@Override
+	protected void init(HierarchicalConfiguration config) throws QueueClientException {
+		checkBaseAddress();
+		checkRelativeAddress();
+		// If config is null, just load the defaults.
+		if(config == null){
+			config = new HierarchicalConfiguration();
+		}
+		this.expire = config.getInt("expire", 0);
+	}
+	
+	private void checkBaseAddress() throws QueueClientException{
+		if(this.getBaseAddress() == null){
+			String errMsg = "Malformed configuration file: No servers defined for KestrelMemcachedClient (baseAddress).";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		}
+	}
+	
+	private void checkRelativeAddress() throws QueueClientException{
+		if(this.getRelativeAddress() == null){
+			String errMsg = "Malformed configuration file: No queue name defined for KestrelMemcachedClient (relativeAddress).";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		}
+	}
+
+}
---
+++ src/main/java/com/servioticy/queueclient/QueueClient.java
@@ -0,0 +1,240 @@
+/*******************************************************************************
+ * Copyright 2014 Barcelona Supercomputing Center (BSC)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/ 
+package com.servioticy.queueclient;
+
+import java.util.List;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
+ * 
+ */
+public abstract class QueueClient {
+
+	final static protected Logger logger = LoggerFactory.getLogger(QueueClient.class);;
+
+	static final private String DEFAULT_CONFIG_PATH = "/conf/default.xml";
+
+	private String baseAddress;
+	private String relativeAddress;
+	private boolean connected;
+
+	protected QueueClient() {
+		
+	}
+
+	private static QueueClient createInstance(String configPath) throws QueueClientException{
+		if(configPath == null){
+			return null;
+		}
+		
+		QueueClient instance;
+		String 	className,
+				type;
+		HierarchicalConfiguration 	config,
+									queueConfig;
+		
+		try {
+			config = new XMLConfiguration(QueueClient.class.getResource(configPath));
+			config.setExpressionEngine(new XPathExpressionEngine());
+
+			if(!config.containsKey("defaultQueue/queueType")){
+				String errMsg = "No default queue. Fix your configuration file.";
+				logger.error(errMsg);
+				throw new QueueClientException(errMsg);
+			}
+			type = config.getString("defaultQueue/queueType");
+			className = config.getString("queue[type='" + type + "']/className");
+			instance = (QueueClient) Class.forName(className).newInstance();
+			instance.setBaseAddress(config.getString("defaultQueue/baseAddress", null));
+			instance.setRelativeAddress(config.getString("defaultQueue/relativeAddress", null));
+			queueConfig = (HierarchicalConfiguration) config
+					.configurationAt("queue[type='" + type + "']/concreteConf");
+			instance.init(queueConfig);
+		} catch (InstantiationException e) {
+			String errMsg = "Unable to instantiate the queue class (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		} catch (IllegalAccessException e) {
+			String errMsg = "Unable to load the queue class (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		} catch (ClassNotFoundException e) {
+			String errMsg = "The queue class does not exist (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		} catch (ConfigurationException e) {
+			String errMsg = "'"+ configPath +"' configuration file is malformed (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		}
+		
+		return instance;
+	}
+	
+	private static QueueClient createInstance(String configPath, String type, String baseAddress, String relativeAddress) throws QueueClientException{
+
+		if(configPath == null){
+			return null;
+		}
+		
+		QueueClient instance;
+		String className;
+		HierarchicalConfiguration	config,
+									queueConfig;
+		try {
+			config = new XMLConfiguration(QueueClient.class.getResource(configPath));
+			config.setExpressionEngine(new XPathExpressionEngine());
+			
+			className = config.getString("queue[type='" + type + "']/className");
+			instance = (QueueClient) Class.forName(className).newInstance();
+			instance.setBaseAddress(baseAddress);
+			instance.setRelativeAddress(relativeAddress);
+			queueConfig = (HierarchicalConfiguration) config
+					.configurationAt("queue[type='" + type + "']/concreteConf");
+			instance.init(queueConfig);
+			
+		} catch (InstantiationException e) {
+			String errMsg = "Unable to instantiate the queue class (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		} catch (IllegalAccessException e) {
+			String errMsg = "Unable to load the queue class (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		} catch (ClassNotFoundException e) {
+			String errMsg = "The queue class does not exist (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		} catch (ConfigurationException e) {
+			String errMsg = "'"+ configPath +"' configuration file is malformed (" + e.getMessage() + ").";
+			logger.error(errMsg);
+			throw new QueueClientException(errMsg);
+		}
+		
+		return instance;
+	}
+	
+	public static QueueClient factory() throws QueueClientException{
+		return createInstance(DEFAULT_CONFIG_PATH);
+	}
+	
+	public static QueueClient factory(String configPath) throws QueueClientException {
+		return createInstance(configPath);
+	}
+	
+	public static QueueClient factory(String qType, String qBaseAddress, String qRelativeAddress) throws QueueClientException{
+		return createInstance(DEFAULT_CONFIG_PATH, qType, qBaseAddress, qRelativeAddress);
+	}
+	
+	public static QueueClient factory(String configPath, String qType, String qBaseAddress, String qRelativeAddress) throws QueueClientException{
+		return createInstance(configPath, qType, qBaseAddress, qRelativeAddress);
+		
+	}
+	
+	public void setBaseAddress(String qBaseAddress){
+		this.baseAddress = qBaseAddress;
+	}
+	
+	public void setRelativeAddress(String qRelativeAddress){
+		this.relativeAddress = qRelativeAddress;
+	}
+	
+	public String getBaseAddress(){
+		return this.baseAddress;
+	}
+	
+	public String getRelativeAddress(){
+		return this.relativeAddress;
+	}
+	
+	public void connect() throws QueueClientException {
+		if (this.isConnected()) {
+			return;
+		}
+
+		this.connectWrapper();
+
+		this.setConnected(true);
+	}
+
+	public void disconnect()  throws QueueClientException{
+		if (!this.isConnected()) {
+			return;
+		}
+
+		this.disconnectWrapper();
+
+		this.setConnected(false);
+	}
+
+	public boolean put(Object item){
+		if (!this.isConnected()) {
+			return false;
+		}	
+		return this.putWrapper(item);
+	}
+	
+	public int put(List<Object> items) {
+		if(items == null || items.size() == 0){
+			return 0;
+		}
+
+		int putCounter = 0;
+		for(; putCounter < items.size(); putCounter++){
+			if(!this.put(items.get(putCounter))){
+				break;
+			}
+		}
+		return putCounter;
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		disconnect();
+		super.finalize();
+	}
+
+	protected abstract boolean putWrapper(Object item);
+
+	protected abstract void connectWrapper() throws QueueClientException;
+
+	protected abstract void disconnectWrapper() throws QueueClientException;
+
+	protected abstract void init(HierarchicalConfiguration config) throws QueueClientException;
+
+	public boolean isConnected() {
+		return this.connected;
+	}
+
+	private void setConnected(boolean connected) {
+		this.connected = connected;
+	}
+
+//	public boolean isTemporalConnection() {
+//		return temporalConnection;
+//	}
+//
+//	public void setTemporalConnection(boolean temporalConnection) {
+//		this.temporalConnection = temporalConnection;
+//	}
+}
